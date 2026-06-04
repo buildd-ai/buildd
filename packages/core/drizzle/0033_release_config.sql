6eecb699d30dfd2314bda0d4c8341c5342980d48
default <ai.run@icloud.com>
2026-06-04 00:34:40 +0000

    chore: renumber release migration to 0040 after rebase onto dev

--- packages/core/drizzle/0033_release_config.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "workspaces" ADD COLUMN "release_config" jsonb;
---> statement-breakpoint
-ALTER TABLE "tasks" ADD COLUMN "release" text DEFAULT 'inherit';
---> statement-breakpoint
-ALTER TABLE "tasks" ADD COLUMN "release_result" jsonb;
